62d258c3839978244d3b3b8b7215217f0a0dbdb1
VLS <vini_ls@hotmail.com>
2026-07-01 13:08:11 -0300

    changing

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,30 @@
-import { View, StyleSheet } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
 import { Example2View } from 'react-native-example2';
 
 export default function App() {
+  const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <AppContent />
       <Example2View color="#32a852" style={styles.box} />
     </View>
   );
 }
+
+function AppContent() {
+  return (
+    <View style={styles2.container}>
+      <Text>Result: </Text>
+    </View>
+  );
+}
+
+const styles2 = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 const styles = StyleSheet.create({
   container: {
